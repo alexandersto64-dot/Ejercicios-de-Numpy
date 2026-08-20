57f77e28c0fa71aae840b0855d06996f54fe9ae8
alexandersto64-dot <alexandersto64@gmail.com>
2026-08-20 09:12:33 -0500

    Ejercicios de Numpy

---
+++ np.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print("NumPy funciona correctamente")
+print("Versión de NumPy:", np.__version__)
+
+numeros = np.array([10, 20, 30, 40, 50])
+
+print("Array:", numeros)
+print("Suma:", np.sum(numeros))
+print("Promedio:", np.mean(numeros))
